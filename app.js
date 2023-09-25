7dd372937c4991a7c5adb10c40a9010cecc03f45
Mr-Babarkhan <muhammadbabarkhan813@gmail.com>
2023-09-25 12:06:27 -0700

    langchain first program

---
+++ app.js
@@ -0,0 +1,11 @@
+import { OpenAI } from "langchain/llms/openai";
+import "dotenv/config";
+const llm = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0.9,
+});
+async function main() {
+    const result = await llm.predict(`10 benefits of gym?`);
+    console.log(result);
+}
+main();
